a99b52d7a7b88a2dd7e92e9b07635827001de79d
Goldenguy4777 <92699543+Goldenguy4777@users.noreply.github.com>
2021-11-29 17:29:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
 <h1 style="text-align:center">Anthony's Microwave</h1>
 <?php
         $servername = "localhost";
@@ -19,11 +22,18 @@
                 $url = '/images/'.$row['image'];
                 $url = str_replace(" ", "%20", $url);
                 $item_page = "item_page.php?item_id=".$row['item_id'];
-                echo "<td><a href = $item_page>
-                      <img src=.$url>";
-                echo '<header>' .$row['name']. '</header><a>';
-                echo '<p>' .$row['description']. '</p>';
-                echo '<p>' .$row['price'].  '</p>';
+                /*echo "<td><a href = $item_page>
+                      <img src=.$url>";*/
+                echo '<td><a href = $item_page><img src="'.$url.' width= '20px' height= '20px'">';
+                
+                //echo '<header>' .$row['name']. '</header><a>';
+                echo "<header style='font-size: 40px; float: left;'> .$row['name']. </header>";    
+                    
+                //echo '<p>' .$row['description']. '</p>';
+                echo '<p class="graybox">' .$row['description']. '</p>';
+                    
+                //echo '<p>' .$row['price'].  '</p>';
+                echo '<p style="font-size: 40px; float: left;">' .$row['price'].  '</p>';
                 echo '</td>';
             }
             else{
