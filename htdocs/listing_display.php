0917d3f79b20c3cea97c6830206c4d73de02c7d8
Goldenguy4777 <92699543+Goldenguy4777@users.noreply.github.com>
2021-11-30 00:07:57 -0800

    Update listing_display.php

--- htdocs/listing_display.php
+++ htdocs/listing_display.php
@@ -3,6 +3,7 @@
     <head>
         <link rel="stylesheet" href="styles.css">
     </head>
+<h1 class="anthonybox"></h1>
 <?php
     $servername = "localhost";
     $username = "root";
@@ -17,19 +18,22 @@
     while ($row = $result -> fetch_assoc()){
         //Size of image is set to 20px of screen, and kept at the right
         echo '<br><img src ="' .$row['image']. ' alt = ' .$row['item_id']. '">';
-        //echo '<br><img src ="' .$row['image']. ' alt = ' .$row['item_id']. ' height= '.20%.' width= '.20%.'">';
-        echo "<img style ='height: 20px; width: 20px; float: right;'>";
         
+        // Keep the name of the item a little bit spaced from the left
         //The name of the product is on the top left, in big font, independent of window
-        //echo '<header>' .$row['name']. '</header>';
-        echo "<header style='font-size: 40px; float: left;'> .$row['name']. </header>";
+        echo "<header style='text-align: left; font-size: 40px; padding-left: 50px;'>" .$row['name']. '</header>';
         
         //The description of the product, encapsulated in a class
-        echo '<p class="graybox">' .$row['description']. '</p>';
+        echo "<p class='imagestxt'>" .$row['description']. '</p>';
         
         //Price, kept at left
-        echo '<p style="font-size: 40px; float: left;">' .$row['price'].  '</p>';
+        echo "<p style='font-size: 20px;
+        text-align: left;
+        height: 15px;
+        width: 50px;
+        padding: 5px;
+        border: 10px;
+        margin: 50px;'>" .$row['price'].  '</p>';
     }
 
 ?>
-</html>
